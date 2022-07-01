f8de6896b9f468b35bae9e572c3eeb3f49bba279
Anil Kukreti <relsellglobal@gmail.com>
2022-07-01 11:51:21 +0530

    added progressbarlib counter intedeterminate false

--- progressbarlib/src/main/java/com/relsellglobal/progressbarlib/ProgressBarFragment.kt
+++ progressbarlib/src/main/java/com/relsellglobal/progressbarlib/ProgressBarFragment.kt
@@ -1,6 +1,8 @@
 package com.relsellglobal.progressbarlib
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,8 @@ class ProgressBarFragment : Fragment(){
 
     lateinit var binding : ProgressLayoutCircularBinding
 
+    var i = 0
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater,R.layout.progress_layout_circular,container,false)
@@ -21,6 +25,24 @@ class ProgressBarFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //binding
+
+        val handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                // set the limitations for the numeric
+                // text under the progress bar
+                if (i <= 100) {
+                    binding.progressText?.setText("" + i)
+                    binding.progressBar?.setProgress(i)
+                    i++
+                    handler.postDelayed(this, 200)
+                } else {
+                    handler.removeCallbacks(this)
+                }
+            }
+        }, 200)
+
+
 
     }
 
